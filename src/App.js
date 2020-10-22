cceb723d49967b58a64bc4a52920591fd520a8e2
egarciam1386 <egarciam1386@gmail.com>
2020-10-21 23:38:05 -0500

    contenido español

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       <h1>React application - This is my app</h1>
+       <h1>Aplicación de react - esta es mi aplicación</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
