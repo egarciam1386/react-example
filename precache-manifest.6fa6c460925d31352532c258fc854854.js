4cb6936125e2400ca8f9493c038b1871c020d1fe
egarciam1386 <egarciam1386@gmail.com>
2020-10-21 23:35:12 -0500

    Updates

---
+++ precache-manifest.6fa6c460925d31352532c258fc854854.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ef8ddbe6865a915c839613de2046be",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "67b3fff89fb65037cb5a",
+    "url": "/react-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad98eef26f25bff9134f",
+    "url": "/react-example/static/js/2.7000064f.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/react-example/static/js/2.7000064f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b3fff89fb65037cb5a",
+    "url": "/react-example/static/js/main.59487b99.chunk.js"
+  },
+  {
+    "revision": "591a35e8bba386688ec8",
+    "url": "/react-example/static/js/runtime-main.c33dc506.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
